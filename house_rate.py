361c8b494bb16ae00503bd1e98fad1ade2593112
Lionkingno12 <astitvath9@gmail.com>
2025-09-17 21:15:32 +0530

    minor changes

--- house_rate.py
+++ house_rate.py
@@ -1,10 +1,10 @@
 import numpy as np
 import copy ,math
 #flow chart (input and output)
-y_axis=np.array([100,200,300,400])
+y_axis=np.array([271.5,232,509.8])
 # y_axis=np.array([300,500])
-x_axis=np.array([[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4]])
-w=np.zeros(x_axis.shape[0])
+x_axis=np.array([[952,2,1,65],[1244,3,2,64],[1974,3,2,17]])
+w=np.zeros(x_axis.shape[1])
 # x_axis=np.array([1,2])
 def func(x,w,b):
     n=np.dot(w,x)+b
@@ -12,12 +12,11 @@ def func(x,w,b):
 #cost function for the program
 def func_cost(w,b,x,y):
     l=x.shape[0]
-    m=np.zeros(l)
     cost_sum=0
     for i in range(l):
         n=np.dot(w,x[i])+b
-        m=(n-y[i])**2
-        cost_sum=cost_sum+m
+        sq_erro=(n-y[i])**2
+        cost_sum=cost_sum+sq_erro
     total_sum=cost_sum/(2*l)
     return total_sum
 #gradient desent
@@ -28,7 +27,7 @@ def gradient(x,b,w,y):
     for i in range(m):
         err=(np.dot(w,x[i])+b)-y[i]
         for j in range(l):
-            jw=jw+(err*x[i,j])/m
+            jw[j]=jw[j]+(err*x[i,j])/m
             jb=jb+(err)/m
     return jw,jb
 # final gradient desent function
@@ -40,6 +39,6 @@ def gradient_desent(w_in,b_in,x,y,nums,alpha):
         b=b-alpha*jb
         w=w-alpha*jw
     return w,b
-print(gradient_desent(w,0,x_axis,y_axis,100,1.0e-2))
-print(func_cost([24.64376465, 24.64376465, 24.64376465, 24.64376465],4.251720410845372,x_axis,y_axis))
-print(func([6,6,6,6],[24.64376465, 24.64376465, 24.64376465, 24.64376465],4.251720410845372))
+print(gradient_desent(w,0,x_axis,y_axis,10000,1e-7))
+print(func_cost([ 0.25325712, -0.01578966, -0.01562902, -0.31193121],-0.000642588207044559,x_axis,y_axis))
+print(func([6874,6,6,60],[ 0.25325712, -0.01578966, -0.01562902, -0.31193121],-0.000642588207044559))
