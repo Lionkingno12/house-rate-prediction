9adc1a4fd4e9f2cf4b8880a9ac0ca0538101bd35
Lionkingno12 <astitvath9@gmail.com>
2025-09-17 21:28:35 +0530

    mini bug fix

--- house_rate.py
+++ house_rate.py
@@ -1,9 +1,9 @@
 import numpy as np
 import copy ,math
 #flow chart (input and output)
-y_axis=np.array([271.5,232,509.8])
+y_axis=np.array([1,2,3,4])
 # y_axis=np.array([300,500])
-x_axis=np.array([[952,2,1,65],[1244,3,2,64],[1974,3,2,17]])
+x_axis=np.array([[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4]])
 w=np.zeros(x_axis.shape[1])
 # x_axis=np.array([1,2])
 def func(x,w,b):
@@ -28,7 +28,7 @@ def gradient(x,b,w,y):
         err=(np.dot(w,x[i])+b)-y[i]
         for j in range(l):
             jw[j]=jw[j]+(err*x[i,j])/m
-            jb=jb+(err)/m
+        jb=jb+(err)/m
     return jw,jb
 # final gradient desent function
 def gradient_desent(w_in,b_in,x,y,nums,alpha):
@@ -39,6 +39,6 @@ def gradient_desent(w_in,b_in,x,y,nums,alpha):
         b=b-alpha*jb
         w=w-alpha*jw
     return w,b
-print(gradient_desent(w,0,x_axis,y_axis,10000,1e-7))
-print(func_cost([ 0.25325712, -0.01578966, -0.01562902, -0.31193121],-0.000642588207044559,x_axis,y_axis))
-print(func([6874,6,6,60],[ 0.25325712, -0.01578966, -0.01562902, -0.31193121],-0.000642588207044559))
+print(gradient_desent(w,0,x_axis,y_axis,10000,1e-3))
+print(func_cost([0.24865072, 0.24865072, 0.24865072, 0.24865072],0.016103867884510077,x_axis,y_axis))
+print(func([6,6,6,6],[0.24865072, 0.24865072, 0.24865072, 0.24865072],0.016103867884510077))
