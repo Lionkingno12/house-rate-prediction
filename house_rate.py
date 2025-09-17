cb1b5193146cd329cec039940ea05593d4de187f
Lionkingno12 <astitvath9@gmail.com>
2025-09-17 22:56:00 +0530

    z normal

--- house_rate.py
+++ house_rate.py
@@ -11,6 +11,7 @@ def func(x,w,b):
     return n
 #cost function for the program
 def func_cost(w,b,x,y):
+    x= z_normalization(x)
     l=x.shape[0]
     cost_sum=0
     for i in range(l):
@@ -21,6 +22,7 @@ def func_cost(w,b,x,y):
     return total_sum
 #gradient desent
 def gradient(x,b,w,y):
+    x=z_normalization(x)
     m,l=x.shape
     jw=np.zeros(l)
     jb=0
@@ -32,6 +34,7 @@ def gradient(x,b,w,y):
     return jw,jb
 # final gradient desent function
 def gradient_desent(w_in,b_in,x,y,nums,alpha):
+    x=z_normalization(x)
     w=copy.deepcopy(w_in)
     b=b_in
     for i in range(nums):
